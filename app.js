2b84981b06a811c1f8880f3e43fca7979737062c
Tia Low <lowtia@gmail.com>
2020-06-09 08:16:15 -0700

    All cities working now

--- app.js
+++ app.js
@@ -56,6 +56,13 @@ var seattleCookies = {
   avgNumCookies : 6.3,
   dailySeattleSales : [] ,
 
+  renderNameToPage: function(){
+    var parentLocationName = document.getElementById('seattle'); //this is target
+    var newHeading = document.createElement('p'); //this creates new element
+    newHeading.textContent = this.name; //this gives new element content
+    parentLocationName.appendChild(newHeading);
+  },
+  
   calculateHourlyCookieSales : function(){
 
     for (var i = 0; i < totalOpenHours.length; i++){
@@ -76,13 +83,7 @@ var seattleCookies = {
     totalCookieList.appendChild(totalCookieItemInList);
   },
 
-  renderNameToPage: function(){
-    var parentLocationName = document.getElementById('seattle'); //this is target
-    var newHeading = document.createElement('p'); //this creates new element
-    newHeading.textContent = this.name; //this gives new element content
-    parentLocationName.appendChild(newHeading);
-  },
-
+ 
   renderHourTotalsToPage: function(){
     var parentLocationHourlySales = document.getElementById('seattle-hours');
     for (var i = 0; i < totalOpenHours.length; i++ ){
@@ -99,7 +100,7 @@ seattleCookies.renderNameToPage();
 seattleCookies.renderHourTotalsToPage();
 seattleCookies.dailyTotalCookieSales();
 
-//---------------------------------------------------------------------- TOKYO OBJECT
+//---------------------------------------------------------------------- TOKYO LOCATION OBJECT
 
 var tokyoCookies = {
   name : 'Tokyo',
@@ -108,6 +109,13 @@ var tokyoCookies = {
   avgNumCookies : 1.2,
   dailyTokyoSales : [],
 
+  renderTokyoNameToPage : function(){
+    var parentLocationNameTokyo = document.getElementById('tokyo');
+    var newHeadingTokyo = document.createElement('p');
+    newHeadingTokyo.textContent = this.name;
+    parentLocationNameTokyo.appendChild(newHeadingTokyo);
+  },
+  
   calculateTokyoHourlyCookieSales : function(){
     
     for (var i = 0; i < totalOpenHours.length; i++){
@@ -117,19 +125,12 @@ var tokyoCookies = {
     }
   },
 
-  dailyTokyoCookieSales : function(){
+  calculateDailyTokyoCookieSales : function(){
     var tokyoCookieTotal = arrSum(this.dailyTokyoSales);
     var tokyoTotalCookieList = document.getElementById('tokyo-hours');
     var tokyoTotalCookieItemInList = document.createElement('li');
     tokyoTotalCookieItemInList.textContent = ('Total: ' + tokyoCookieTotal + ' cookies');
     tokyoTotalCookieList.appendChild(tokyoTotalCookieItemInList);
-  },
-
-  renderTokyoNameToPage : function(){
-    var parentLocationNameTokyo = document.getElementById('tokyo');
-    var newHeadingTokyo = document.createElement('p');
-    newHeadingTokyo.textContent = this.name;
-    parentLocationNameTokyo.appendChild(newHeadingTokyo);
   },
 
   renderTokyoHourTotalToPage : function(){
@@ -147,7 +148,157 @@ var tokyoCookies = {
 tokyoCookies.calculateTokyoHourlyCookieSales();
 tokyoCookies.renderTokyoNameToPage();
 tokyoCookies.renderTokyoHourTotalToPage();
-tokyoCookies.dailyTokyoCookieSales();
+tokyoCookies.calculateDailyTokyoCookieSales();
+
+//----------------------------------------------------------------------------- DUBAI LOCATION OBJECT
+
+var dubaiCookies = {
+  name : "Dubai",
+  minNumCustomers: 11,
+  maxNumCustomers: 38,
+  avgNumCookies: 3.7,
+  dailyDubaiSales: [],
+
+  renderDubaiNameToPage : function(){
+    var parentLocationNameDubai = document.getElementById('dubai');
+    var newParagraphDubai = document.createElement('p');
+    newParagraphDubai.textContent = this.name;
+    parentLocationNameDubai.appendChild(newParagraphDubai);
+  },
+
+  calculateDubaiHourlyCookieSales : function(){
+    for (var i = 0; i < totalOpenHours.length; i++){
+      var randomNumber = getRandomNumCustomers(this.minNumCustomers, this.maxNumCustomers);
+      var totalHourlySalesDubai = Math.round(randomNumber * this.avgNumCookies);
+      this.dailyDubaiSales.push(totalHourlySalesDubai);
+    }
+  },
+
+  calculateDailyDubaiCookieSales : function(){
+    var dubaiCookieTotal = arrSum(this.dailyDubaiSales);
+    var dubaiTotalCookieList = document.getElementById('dubai-hours');
+    var dubaiTotalCookieListItem = document.createElement('li');
+    dubaiTotalCookieListItem.textContent = ('Total: ' + dubaiCookieTotal + ' cookies');
+    dubaiTotalCookieList.appendChild(dubaiTotalCookieListItem);
+  },
+
+  renderDubaiHourTotalsToPage : function(){
+    var parentDubaiLocationHourSales = document.getElementById('dubai-hours');
+    for (var i = 0; i < totalOpenHours.length; i++){
+      var dubaiLocationListItem = document.createElement('li');
+      dubaiLocationListItem.textContent = totalOpenHours[i] + ': ' + this.dailyDubaiSales[i] + ' cookies';
+      parentDubaiLocationHourSales.appendChild(dubaiLocationListItem);
+    };
+  },
+};
+
+dubaiCookies.renderDubaiNameToPage();
+dubaiCookies.calculateDubaiHourlyCookieSales();
+dubaiCookies.renderDubaiHourTotalsToPage();
+dubaiCookies.calculateDailyDubaiCookieSales();
 
 
+
+//----------------------------------------------------------------------------- PARIS LOCATION OBJECT
+
+var parisCookies = {
+  name : "Paris",
+  minNumCustomers: 20,
+  maxNumCustomers: 38,
+  avgNumCookies: 2.3,
+  dailyParisSales: [],
+
+  renderParisNameToPage : function(){
+    var parentLocationNameParis = document.getElementById('paris');
+    var newParagraphParis = document.createElement('p');
+    newParagraphParis.textContent = this.name;
+    parentLocationNameParis.appendChild(newParagraphParis);
+  },
+
+  calculateParisHourlySales : function(){
+    for (var i = 0; i < totalOpenHours.length; i++){
+      var randomNumber = getRandomNumCustomers(this.minNumCustomers, this.maxNumCustomers);
+      var totalHourlySalesParis = Math.round(randomNumber * this.avgNumCookies);
+      this.dailyParisSales.push(totalHourlySalesParis);
+    }
+  },
+
+  calculateDailyParisCookieSales : function(){
+    var parisCookieTotal = arrSum(this.dailyParisSales);
+    var parisTotalCookieList = document.getElementById('paris-hours');
+    var parisTotalCookieListItem = document.createElement('li');
+    parisTotalCookieListItem.textContent = ('Total: ' + parisCookieTotal + ' cookies');
+    parisTotalCookieList.appendChild(parisTotalCookieListItem);
+  },
+
+  renderParisHourTotalsToPage : function(){
+    var parentParisLocationHourSales = document.getElementById('paris-hours');
+    for (var i = 0; i < totalOpenHours.length; i++){
+      var parisLocationListItem = document.createElement('li');
+      parisLocationListItem.textContent = totalOpenHours[i] + ': ' + this.dailyParisSales[i] + ' cookies';
+      parentParisLocationHourSales.appendChild(parisLocationListItem);
+    }
+  },
+}
+
+
+
+parisCookies.renderParisNameToPage();
+parisCookies.calculateParisHourlySales();
+parisCookies.renderParisHourTotalsToPage();
+parisCookies.calculateDailyParisCookieSales();
+
+
+
+
+
+
+//----------------------------------------------------------------------------- LIMA LOCATION OBJECT
+
+var limaCookies = {
+  name : "Lima",
+  minNumCustomers: 2,
+  maxNumCustomers: 16,
+  avgNumCookies: 4.6,
+  dailyLimaSales: [],
+
+  renderLimaNameToPage : function(){
+    var parentLocationNameLima = document.getElementById('lima');
+    var newParagraphLima = document.createElement('p');
+    newParagraphLima.textContent = this.name;
+    parentLocationNameLima.appendChild(newParagraphLima);
+  },
+
+  calculateLimaHourlySales : function(){
+    for (var i = 0; i < totalOpenHours.length; i++){
+      var randomNumber = getRandomNumCustomers(this.minNumCustomers, this.maxNumCustomers);
+      var totalHourlySalesLima = Math.round(randomNumber * this.avgNumCookies);
+      this.dailyLimaSales.push(totalHourlySalesLima);
+    }
+  },
+
+  calculateDailyLimaCookieSales : function(){
+    var limaCookieTotal = arrSum(this.dailyLimaSales);
+    var limaTotalCookieList = document.getElementById('lima-hours');
+    var limaTotalCookieListItem = document.createElement('li');
+    limaTotalCookieListItem.textContent = ('Total: ' + limaCookieTotal + ' cookies');
+    limaTotalCookieList.appendChild(limaTotalCookieListItem);
+  },
+
+  renderLimaHourTotalsToPage : function(){
+    var parentLimaLocationHourSales = document.getElementById('lima-hours');
+    for (var i = 0; i < totalOpenHours.length; i++){
+      var limaLocationListItem = document.createElement('li');
+      limaLocationListItem.textContent = totalOpenHours[i] + ': ' + this.dailyLimaSales[i] + ' cookies';
+      parentLimaLocationHourSales.appendChild(limaLocationListItem);
+    }
+  },
+}
+
+
+
+limaCookies.renderLimaNameToPage();
+limaCookies.calculateLimaHourlySales();
+limaCookies.renderLimaHourTotalsToPage();
+limaCookies.calculateDailyLimaCookieSales();
 
